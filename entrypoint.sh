a6f9330a0fe1018f7ec491b4e9c55c061daae898
Gernot Schulz <gernot@intevation.de>
2021-09-07 12:54:02 +0200

    entrypoint: Refuse to start up if migrations missing

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,37 @@ export DATASTORE_DATABASE_PASSWORD=${DATASTORE_DATABASE_PASSWORD:-openslides}
 
 ./wait.sh $DATASTORE_WRITER_HOST $DATASTORE_WRITER_PORT
 
+max_retries=10
+sleep_time=10
+
+marker=/tmp/migration-in-progress
+
+# Test if migrations are necessary and refuse to start until they have been
+# applied.
+while true; do
+  # Detect if migrations have been started in this particular container.  If so,
+  # don't do anything until they have finished.
+  #
+  # TODO: This is a workaround.  Ideally, migrate.py could be used to detect
+  # in-progress migrations in the future.
+  if [[ -e "$marker" ]]; then
+    echo "INFO: Migrations in progress (${marker} exists). Waiting indefinitely."
+    count=
+  else
+    python migrations/migrate.py stats
+    python migrations/migrate.py status && break
+    err_code=$?
+    if [[ $err_code -eq 1 ]]; then
+      echo "WARN: Migrations necessary."
+    elif [[ $err_code -eq 2 ]]; then
+      echo "ERROR: Migrations impossible."
+    else
+      echo "ERROR: Unknown error: $err_code"
+      exit $err_code
+    fi
+    [[ $(( ++count )) -lt $max_retries ]] || exit $err_code
+  fi
+  sleep $sleep_time
+done
+
 exec "$@"
