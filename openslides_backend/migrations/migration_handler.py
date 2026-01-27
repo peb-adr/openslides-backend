8a79394421adf5b60e476300395631bba5b45b4c
Adrian Richter <adrian@intevation.de>
2026-01-27 20:22:01 +0100

    Add "EXEC: " prefix

--- openslides_backend/migrations/migration_handler.py
+++ openslides_backend/migrations/migration_handler.py
@@ -44,7 +44,7 @@ class MigrationHandler(BaseHandler):
                 table_m=table_m, table_t=table_t
             )
         )
-        print((self.cursor._query.query or b"").decode("utf-8"))
+        print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
         # TODO we might need finalization tables for future migrations to have active triggers on the table.
 
@@ -71,7 +71,7 @@ class MigrationHandler(BaseHandler):
                 ),
             )
         )
-        print((self.cursor._query.query or b"").decode("utf-8"))
+        print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
     def setup_migration_relations(self) -> None:
         """Sets the tables and views used within the migration and copies their data."""
@@ -156,7 +156,7 @@ class MigrationHandler(BaseHandler):
                         ),
                     )
                 )
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
         def replace_suffix(m: re.Match) -> str:
             base = m.group(1)
@@ -185,7 +185,7 @@ class MigrationHandler(BaseHandler):
                     viewdef=sql.SQL(viewdef),
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
             # TODO rereference all models pointing to or pointed from this collection including im tables
             # (not origin tables)
             # shouldn't that be done during finalize?
@@ -237,7 +237,7 @@ class MigrationHandler(BaseHandler):
                 replaced_blocks.append(view_re.sub(add_suffix, modified_block))
         sql_text = "".join(replaced_blocks)
         self.cursor.execute(sql_text)
-        print((self.cursor._query.query or b"").decode("utf-8"))
+        print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
     def update_sequence(self, name: str, maximum: int) -> None:
         self.cursor.execute(
@@ -246,7 +246,7 @@ class MigrationHandler(BaseHandler):
                 maximum=maximum,
             )
         )
-        print((self.cursor._query.query or b"").decode("utf-8"))
+        print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
     def update_sequences(self) -> None:
         """
@@ -282,7 +282,7 @@ class MigrationHandler(BaseHandler):
                     self.cursor.execute(
                         sql.SQL(f"CREATE SEQUENCE IF NOT EXISTS {seq_name};")
                     )
-                    print((self.cursor._query.query or b"").decode("utf-8"))
+                    print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
                     self.update_sequence(seq_name, result["max"])
 
     def execute_migrations(self) -> None:
@@ -383,7 +383,7 @@ class MigrationHandler(BaseHandler):
                         table=sql.Identifier(table),
                     )
                 )
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
     def unset_tables_read_only(self) -> None:
         """Sets all origin_collections tables to readable by dropping the read-only trigger."""
@@ -393,7 +393,7 @@ class MigrationHandler(BaseHandler):
                     trigger_name=sql.SQL(f"tr_lock_{table}")
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
         # Support reset on initial migration.
         if MigrationHelper.get_database_migration_index(self.cursor) < 100:
             for table in OLD_TABLES:
@@ -402,7 +402,7 @@ class MigrationHandler(BaseHandler):
                         table=sql.SQL(table)
                     )
                 )
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
     @classmethod
     def close_migrate_thread_stream(cls) -> str:
@@ -460,27 +460,27 @@ class MigrationHandler(BaseHandler):
                     real_name=sql.Identifier(collection + "_t")
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
             self.cursor.execute(
                 sql.SQL("ALTER TABLE {migration_name} RENAME TO {real_name}").format(
                     real_name=sql.Identifier(collection + "_t"),
                     migration_name=sql.Identifier(migration_names["table"]),
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
             self.cursor.execute(
                 sql.SQL(
                     "ALTER SEQUENCE {collection}_m_id_seq RENAME TO {collection}_t_id_seq;"
                 ).format(collection=sql.SQL(collection))
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
             # Will be recreated for origin table below.
             self.cursor.execute(
                 sql.SQL("DROP VIEW {migration_name};").format(
                     migration_name=sql.Identifier(migration_names["view"]),
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
             im_tables.update(migration_names["im_tables"])
 
         # RENAME intermediate tables
@@ -490,7 +490,7 @@ class MigrationHandler(BaseHandler):
                     real_name=sql.Identifier(table_name)
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
             self.cursor.execute(
                 sql.SQL("ALTER TABLE {migration_name} RENAME TO {real_name}").format(
                     real_name=sql.Identifier(table_name),
@@ -499,7 +499,7 @@ class MigrationHandler(BaseHandler):
                     ),
                 )
             )
-            print((self.cursor._query.query or b"").decode("utf-8"))
+            print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
         # RECREATE triggers
         if HelperGetNames.trigger_unique_list:
@@ -550,9 +550,9 @@ class MigrationHandler(BaseHandler):
                         table=sql.Identifier(trigger_dict["table_name"]),
                     )
                 )
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
         self.cursor.execute(sql_text)
-        print((self.cursor._query.query or b"").decode("utf-8"))
+        print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
 
         self.update_sequences()
 
@@ -586,7 +586,7 @@ class MigrationHandler(BaseHandler):
             query,
             (to_delete_indices,),
         )
-        print(self.cursor.mogrify(query, (to_delete_indices,)))
+        print("EXEC: " + self.cursor.mogrify(query, (to_delete_indices,)))
 
         self.unset_tables_read_only()
 
@@ -611,10 +611,10 @@ class MigrationHandler(BaseHandler):
         if MigrationHelper.get_database_migration_index(self.cursor) < 100:
             for collection in replace_tables:
                 self.cursor.execute(f"DROP TABLE {collection}_t;")
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
         if any(mi > 100 for mi in indices):
             for table_view in replace_tables.values():
                 self.cursor.execute(f"DROP TABLE {table_view['table']};")
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
                 self.cursor.execute(f"DROP VIEW {table_view['view']};")
-                print((self.cursor._query.query or b"").decode("utf-8"))
+                print("EXEC: " + (self.cursor._query.query or b"").decode("utf-8"))
