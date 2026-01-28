3c19d1e51041f46576f96050a8007e760d483baa
Adrian Richter <adrian@intevation.de>
2026-01-28 10:58:08 +0100

    Revert "Add "EXEC: " prefix"
    This reverts commit 8a79394421adf5b60e476300395631bba5b45b4c.

--- openslides_backend/migrations/migration_helper.py
+++ openslides_backend/migrations/migration_helper.py
@@ -239,7 +239,7 @@ class MigrationHelper:
             updates=sql.SQL(", ").join(updates),
         )
         curs.execute(statement, tuple(params.values()))
-        print("EXEC: " + curs.mogrify(statement, tuple(params.values())))
+        print(curs.mogrify(statement, tuple(params.values())))
         curs.connection.commit()
 
     @staticmethod
--- openslides_backend/migrations/migrations/0100_init_reldb.py
+++ openslides_backend/migrations/migrations/0100_init_reldb.py
@@ -354,7 +354,7 @@ def data_manipulation(curs: Cursor[DictRow]) -> None:
                 query,
                 sql_values,
             )
-            print("EXEC: " + curs.mogrify(query, sql_values))
+            print(curs.mogrify(query, sql_values))
         # END LOOP data_rows
         MigrationHelper.write_line(
             f"{min(Sql_helper.offset, models_count)} of {models_count} models written to tables."
@@ -364,7 +364,7 @@ def data_manipulation(curs: Cursor[DictRow]) -> None:
     # 4) INSERT intermediate tables
     for command, values in insert_intermediate_t_commands:
         curs.execute(command, values)
-        print("EXEC: " + curs.mogrify(command, values))
+        print(curs.mogrify(command, values))
 
     # clear replace tables as this migration writes the tables directly
     MigrationHelper.set_database_migration_info(
@@ -383,7 +383,7 @@ def cleanup(curs: Cursor[DictRow]) -> None:
     """
     for table_name in OLD_TABLES:
         curs.execute(f"DROP TABLE {table_name} CASCADE;")
-        print("EXEC: " + (curs._query.query or b"").decode("utf-8"))
+        print((curs._query.query or b"").decode("utf-8"))
 
 
 
